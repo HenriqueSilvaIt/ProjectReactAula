c18db7bbbdfcf2b5a04bbf5ba4c6d2026d617bfb
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-02-18 09:07:25 -0300

    Project rederization

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import computerImg from './assets/compute.png' /*importantando imagem*/
+import cartImg from './assets/cart.svg'/*importantando imagem*/
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+  return ( /* esse <> </> é um fragment, sempre que tenho mais de 1 elemento html eu preciso colocar 
+    dentro de um fragment, porque eu só posso ter 1 elemento java script de retorno que é um fragment */
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="dsc-header-client">
+        <nav className="dsc-container">
+          <h1>E-commerce</h1>
+          <div className="dsc-nav-bar-right">
+            <div className="dsc-menu-itens-container"> 
+              <div className="dsc-menu-item">
+                <img src={cartImg} alt="car" />
+              </div>
+            </div>
+            <a href="#">Entrar</a>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <section id="product-details-section" className="dsc-container">
+          <div className="dsc-card dsc-mb20">
+            <div className="dsc-product-details-top dsc-line-bottom">
+              <img src={computerImg} alt="Computer" />
+            </div>
+            <div className="dsc-product-details-bottom">
+              <h3>R$ 5000,00</h3>
+              <h4>Computador Gamer XT</h4>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla
+                aliquam neque incidunt aperiam, magnam dolorum et, facere officiis
+                ad iste accusantium ipsam impedit nesciunt delectus obcaecati
+                nihil, assumenda natus sint?
+              </p>
+              <div className="dsc-category-container">
+                <div className="dsc-category">
+                  Eletronicos
+                </div>
+                <div className="dsc-category">Computadores</div>
+              </div>
+            </div>
+          </div>
+          <div className="dsc-btn-page-container">
+            <div className="dsc-btn dsc-btn-blue">
+              Comprar
+            </div>
+            <div className="dsc-btn dsc-btn-white">
+              Início
+            </div>
+          </div>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App /> 
   </StrictMode>,
-)
+) /*esse componente app dentro do strictmode é a função que está dentro do arquivo App.tsx
+e dentro dessa função tem um h1*/
