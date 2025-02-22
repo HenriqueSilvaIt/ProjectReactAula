419e510c297cae4e8934e7c4717a1cea3cf05a2c
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-02-22 09:46:44 -0300

    Not Found Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Catalog from './routes/ClientHome/Catalog';
 import ProductDetails from './routes/ProductDetails';
 import ClientHome from './routes/ClientHome';
@@ -20,6 +20,7 @@ o BrowserRouter, Routes e  Route e por padrão temos que colocar
       <Route  index element={<Catalog/>}/>
       <Route path="catalog" element={<Catalog/>}/>
         <Route path="product-details/:productId" element={<ProductDetails/>} />
+        <Route path="*" element={<Navigate to="/"/>} />
       </Route>
       </Routes>
       </BrowserRouter>
