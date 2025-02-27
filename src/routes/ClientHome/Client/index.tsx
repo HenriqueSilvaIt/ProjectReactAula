8925442a406c0b97363aac1f58c9a4ebfe836260
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-02-27 07:29:46 -0300

    Cart mock data

--- src/routes/ClientHome/Client/index.tsx
+++ src/routes/ClientHome/Client/index.tsx
@@ -1,48 +1,71 @@
 import './style.css';
 import computerImg from '../../../assets/compute.png';
 
+
+const cart = {
+    items: [
+        {
+            productId: 4,
+            quantity: 1,
+            name: "PC Gamer",
+            price: 1200,
+            imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg"
+        },
+        {
+            productId: 5,
+            quantity: 2,
+            name: "Rails for Dummies",
+            price: 100.99,
+            imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/5-big.jpg"
+        },
+        {
+            productId: 5,
+            quantity: 2,
+            name: "Rails for Dummies",
+            price: 100.99,
+            imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/5-big.jpg"
+        }
+    ]
+}
+
 export default function Cart() {
 
-    return (
+    return (/* quando abrimos chaves dentro do return é uma expressão do react */
+        /* no primeiro elemento dentro da função map tem que colocar o key
+        que ai pegamos o id do objeto porrque tem que ser um elemento único */
         <main>
-            <section id="cart-container-section" className="dsc-container">
+            <section  id="cart-container-section" className="dsc-container">
                 <div className="dsc-card dsc-mb20">
-                    <div className="dsc-cart-item-container dsc-line-bottom">
-                        <div className="dsc-cart-item-left">
-                            <img src={computerImg} alt="computer" />
-                            <div className="dsc-cart-item-description">
-                                <h3>Computador Gamer XT</h3>
-                                <div className="dsc-cart-item-quantity-container">
-                                    <div className="dsc-cart-item-quantity-btn">-</div>
-                                    <p>1</p>
-                                    <div className="dsc-cart-item-quantity-btn">+</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="dsc-cart-item-right">R$ 5000,00</div>
-                    </div>
 
-                    <div className="dsc-cart-item-container dsc-line-bottom">
-                        <div className="dsc-cart-item-left">
-                            <img src={computerImg} alt="computer" />
-                            <div className="dsc-cart-item-description">
-                                <h3>Computador Gamer XT</h3>
-                                <div className="dsc-cart-item-quantity-container">
-                                    <div className="dsc-cart-item-quantity-btn">-</div>
-                                    <p>1</p>
-                                    <div className="dsc-cart-item-quantity-btn">+</div>
+                    {cart.items.map(item => (
+
+                        <div key={item.productId} className="dsc-cart-item-container dsc-line-bottom">
+                            <div className="dsc-cart-item-left">
+                                <img src={item.imgUrl} alt={item.name} />
+                                <div className="dsc-cart-item-description">
+                                    <h3>{item.name}</h3>
+                                    <div className="dsc-cart-item-quantity-container">
+                                        <div className="dsc-cart-item-quantity-btn">-</div>
+                                        <p>{item.quantity}</p>
+                                        <div className="dsc-cart-item-quantity-btn">+</div>
+                                    </div>
                                 </div>
                             </div>
+                            <div className="dsc-cart-item-right">R$ {(item.price * item.quantity).toFixed(2)}</div>
                         </div>
-                        <div className="dsc-cart-item-right">R$ 5000,00</div>
-                    </div>
+                    ))}
+
+
+
+
+
                     <div className="dsc-cart-total-container">
                         <h4>Total:</h4>
                         <h3>R$ 1500,00</h3>
                     </div>
                 </div>
                 <div className="dsc-btn-page-container">
-                   
+
                     <div className="dsc-btn dsc-btn-blue">
                         Finalizar Pedido
                     </div>
