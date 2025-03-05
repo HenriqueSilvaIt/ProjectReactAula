5b65c1ebf9619087b9233310e45f994337255c49
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-03-05 07:29:58 -0300

    Layou Form

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -1,8 +1,46 @@
+import './style.css';
 
 export default function ProductForm() {
 
     return (
-        <h2>  l</h2>
+        <main>
+            <section id="product-form-section" className="dsc-container">
+                <div className="dsc-product-form-container">
+                    <form className="dsc-card dsc-form">
+                        <h2>Dados do produto</h2>
+                        <div className="dsc-form-controls-container">
+                            <div>
+                                <input className="dsc-form-control" type="text" placeholder="Nome"/>
+                            </div>
+                            <div>
+                                <input className="dsc-form-control" type="text" placeholder="Preço"/>
+                            </div>
+                            <div>
+                                <input className="dsc-form-control" type="text" placeholder="Imagem"/>
+                            </div>
+                            <div>
+                                <select className="dsc-form-control dsc-select" required> 
+                                    <option value="" disabled selected>Categorias</option>
+                                    <option value="">Valor 1</option>
+                                    <option value="">Valor 2</option>
+                                </select>
+                            </div>
+                            <div>
+                                <textarea className="dsc-form-control dsc-textarea">
+
+                                </textarea>
+                            </div>
+                        </div>
+
+                        <div className="dsc-product-form-buttons">
+                            <button type="reset" className="dsc-btn dsc-btn-white">Cancelar</button>
+                            <button type="submit" className="dsc-btn dsc-btn-blue">Salvar</button>
+                        </div>
+                    </form>
+                </div>
+            </section>
+        </main>
+
 
     )
 } 
