3ff431dfd61e877df3d9d26ee96557e605aa5ad8
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-03-06 09:11:54 -0300

    Product route editing product

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -1,7 +1,68 @@
-import { Link } from 'react-router-dom';
 import './style.css';
+import { Link, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import FormInput from '../../../components/FormInput';
+import * as forms from '../../../utils/form';
+import * as productervice from '../../../services/product-services';
 
 export default function ProductForm() {
+
+    const params = useParams(); /* para colocara rota ul */
+
+    const isEditing = params.productId !== 'create'; /* se a rota for dirente de create significa que estou editando um
+    produto e não criando um novo, se for create está editando  */
+
+  const [formData, setFormData] = useState<any>({ /* any é para o type script
+        n reclemar dos valores, para objeto ser um objeto livre e ter qualquer atributo dentro dele
+        de qualquer tipo */
+        /* os objetoa abaixo, vai ser igual o objeto do product by id do postman*/
+        name: {
+            value: "",
+            id: "name",
+            name: "name",
+            type: "text",
+            placeholder: "Nome",
+        },
+        price: {
+            value: "",
+            id: "price",
+            name: "price",
+            type: "number", /* para aceitar somente númeroes*/
+            placeholder: "Preço",
+        },
+        imgUrl: {
+            value: "",
+            id: "imgUrl",
+            name: "imgUrl",
+            type: "text",
+            placeholder: "Imagem",
+        }
+    })
+
+
+    useEffect(() => {
+        if(isEditing)/* se for verdade o iediting*/ {
+            productervice.findById(Number(params.productId)) /*number para converter para número
+            se n vai reclamara*/ 
+                .then(response => {
+                    console.log(response.data);
+                })
+        }
+    }, [])
+
+
+     function handleInputChange(event: any) {
+      
+            setFormData(forms.update(formData/*objeto já com as informações do input */, event.target.name/*campo da cainha
+                do input que estou mexendo*/, event.target.value/*valor que
+                digitar*/)); /* destruturamos
+            o formData para aproveitar o que tinha nele e onde tem o cmapo com o nome name
+            vamos colocar o novo valor value que  está digitado 
+            
+            Agora novo formulário temos que preservar todo objeto, então vamos pegar tudo que já tinha no objeto, porém no campo
+       value, vamos colocar o value criado na função event.target.valu*/
+        }
+
 
     return (
         <main>
@@ -11,13 +72,22 @@ export default function ProductForm() {
                         <h2>Dados do produto</h2>
                         <div className="dsc-form-controls-container">
                             <div>
-                                <input className="dsc-form-control" type="text" placeholder="Nome"/>
+                                <FormInput {...formData.name} 
+                                className="dsc-form-control"
+                                onChange={handleInputChange} 
+                                />
                             </div>
                             <div>
-                                <input className="dsc-form-control" type="text" placeholder="Preço"/>
+                            <FormInput {...formData.price} 
+                                className="dsc-form-control"
+                                onChange={handleInputChange} 
+                                />
                             </div>
                             <div>
-                                <input className="dsc-form-control" type="text" placeholder="Imagem"/>
+                            <FormInput {...formData.imgUrl} 
+                                className="dsc-form-control"
+                                onChange={handleInputChange} 
+                                />
                             </div>
                         </div>
 
