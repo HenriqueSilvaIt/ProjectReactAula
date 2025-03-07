23da9735ae5903a3dba131c8ecaf8800d6696446
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-03-07 12:15:23 -0300

    Deploy ready

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import React from 'react'
+
+
+window.React = React
 
 createRoot(document.getElementById('root')!).render(  /* selecionsa o elemento html que tem o root e renderiza dentro dele o resultado processamento do react */
 
--- src/utils/system.ts
+++ src/utils/system.ts
@@ -1,10 +1,13 @@
 
-export const BASE_URL= "http://localhost:8090";
-
 export const CART_KEY= "com.devsuperior.ecommerce/Cart";
-
-export const CLIENT_ID = "myclientid";
-
-export const CLIENT_SECRET = "myclientsecret";
-
 export const TOKEN_KEy = "com.devsuperior.ecommerce/art";
+
+
+/* coloca variavel de ambiente e caso n esteja configura ??(operador de coalesencia nula, se n existir
+ variavel da esquerda pega por padrão o valo da direita) colcoa um valor padrão no lugar */
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8090"; /* se 
+// tiver configurado na ferramenta na nuvem a variavel, por padrão tem que ser pega
+// diretóreto de lá,  mas com oestamos rodando na máquina vamos coloc aqu 
+// */
+export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID ?? "myclientid";
+export const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET ?? "myclientsecret";
