5d07107aa44fe49430f3ff6d650775708e533c52
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-03-07 08:42:01 -0300

    Categories with backend

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -4,7 +4,12 @@ import { useEffect, useState } from 'react';
 import FormInput from '../../../components/FormInput';
 import * as forms from '../../../utils/form';
 import * as productervice from '../../../services/product-services';
+import * as categoryService from '../../../services/category-service';
 import FormTextArea from '../../../components/FormTextArea';
+import Select from 'react-select';
+import { CategoryDTO } from '../../../models/category';
+/*o import é só import Select from 'react-select';, se você importa automatico vai trazer errado
+ tem que fica igual no import da documentação ficial*/
 
 export default function ProductForm() {
 
@@ -13,7 +18,7 @@ export default function ProductForm() {
     const isEditing = params.productId !== 'create'; /* se a rota for dirente de create significa que estou editando um
     produto e não criando um novo, se for create está editando  */
 
-  const [formData, setFormData] = useState<any>({ /* any é para o type script
+    const [formData, setFormData] = useState<any>({ /* any é para o type script
         n reclemar dos valores, para objeto ser um objeto livre e ter qualquer atributo dentro dele
         de qualquer tipo */
         /* os objetoa abaixo, vai ser igual o objeto do product by id do postman*/
@@ -23,7 +28,7 @@ export default function ProductForm() {
             name: "name",
             type: "text",
             placeholder: "Nome",
-            validation: function(value: string) {
+            validation: function (value: string) {
                 return /^.{3,80}$/.test(value);
             },
             message: "Favor informar um nome de 3 a 80 caracteres"
@@ -34,7 +39,7 @@ export default function ProductForm() {
             name: "price",
             type: "number", /* para aceitar somente númeroes*/
             placeholder: "Preço",
-            validation: function(value: any) {
+            validation: function (value: any) {
                 return Number(value)/*v convertido para Number*/ > 0;
             },
             /*messagem de erro caso essa função de falso*/
@@ -47,43 +52,54 @@ export default function ProductForm() {
             type: "text",
             placeholder: "Imagem",
         },
-        description: {  
-            
+        description: {
+
             value: "",
             id: "description",
             name: "description",
             type: "text",
             placeholder: "Descrição",
-            validation: function(value: string) {
+            validation: function (value: string) {
                 return /^.{10,}$/.test(value); /* expressão regez que pega o mínimo 10 caractere */
             },
-              message: "Favor informar um nome de 3 a 80 caracteres"
-         
+            message: "Favor informar um nome de 3 a 80 caracteres"
+
         }
-});
+    });
+
+
+    /*useEffect para carregar as categoria*/
+
+    useEffect(() => {
+        categoryService.findAllRequest() /* para buscar a lita de categoria do bakcend*/
+            .then(response => {
+                setCategories(response.data) /* colocando as categorias
+                no useStat*/
+            })
+    }, []);
 
 
     useEffect(() => {
 
-        if(isEditing)/* se for verdade o iediting quer
+        if (isEditing)/* se for verdade o iediting quer
         dizer que a rota n é create, é de edição de produto */ {
             productervice.findById(Number(params.productId)) /*number para converter para número
-            se n vai reclamara*/ 
+            se n vai reclamara*/
                 .then(response => {
                     console.log(response.data); /* vai retornar o produto(objeto)
                     ou seja se for uma rota diferente de create, ele vai trazer o formulário
                     já com os dados do produto preenchido porque é editação*/
-                    const newFormData = forms.updateAll(formData,response.data)
-                   setFormData(newFormData); /*deixando os campos fo formulário já preenchido -  gerando
+                    const newFormData = forms.updateAll(formData, response.data)
+                    setFormData(newFormData); /*deixando os campos fo formulário já preenchido -  gerando
                     um novo objeto e no campo value vai colocar o valor que estava no banco de dados */
                 })
         }
     }, [])
 
 
-     function handleInputChange(event: any) {
+    function handleInputChange(event: any) {
 
-        
+
         /* chamando função para atualizar oque o usuário ta escrevendo no input
 
            const dataUpdate = forms.update(formData/*objeto já com as informações do input , event.target.name/*campo da cainha
@@ -96,21 +112,29 @@ export default function ProductForm() {
    value, vamos colocar o value criado na função event.target.valu*/
 
 
-             /*chamando função que valida se o campo foi preenchido corretamente*/
-    
-           ; /* 1º argumento
+        /*chamando função que valida se o campo foi preenchido corretamente*/
+
+        ; /* 1º argumento
              oque nós digitamos no campo, 2º argumento o nome do campo (exemplo price, name et)*/
 
-            setFormData(forms.updateAndValidate(formData, event.target.name, event.target.value)); /* recebe oque tá sendo  digitado no input
+        setFormData(forms.updateAndValidate(formData, event.target.name, event.target.value)); /* recebe oque tá sendo  digitado no input
             que é q primeira função dataUpdate e valdia se for executado corretamente
             que é e a segunda função dataValited  (dataUpdate já está dentro da dataValited),
             por que aqui só colocamos  dataValited*/
-        }
+    }
 
-        function handleTurnDirty(name: string) {
-            setFormData(forms.dirtAndValidate(formData, name)); /* agora 
+    function handleTurnDirty(name: string) {
+        setFormData(forms.dirtAndValidate(formData, name)); /* agora 
             com função dirty and validate, ele fica vermelho */
-        }
+    } 
+
+    const [categories, setCategories] = useState<CategoryDTO>();
+
+    const options = [
+        {value: 'chocolate', label: 'Chocolate' },
+        {value: 'strawberry', label: 'Strawberry' },
+        {value: 'vanilla', label: 'Vanilla' }
+        ]
 
     return (
         <main>
@@ -120,33 +144,41 @@ export default function ProductForm() {
                         <h2>Dados do produto</h2>
                         <div className="dsc-form-controls-container">
                             <div>
-                                <FormInput {...formData.name} 
-                                className="dsc-form-control"
-                                onTurnDirty={handleTurnDirty}
-                                onChange={handleInputChange} 
+                                <FormInput {...formData.name}
+                                    className="dsc-form-control"
+                                    onTurnDirty={handleTurnDirty}
+                                    onChange={handleInputChange}
                                 />
                                 <div className="dsc-form-error">{formData.name.message}</div>
                             </div>
                             <div>
-                            <FormInput {...formData.price} 
-                                className="dsc-form-control"
-                                onChange={handleInputChange}
-                                onTurnDirty={handleTurnDirty} 
+                                <FormInput {...formData.price}
+                                    className="dsc-form-control"
+                                    onChange={handleInputChange}
+                                    onTurnDirty={handleTurnDirty}
                                 />
-                               <div className="dsc-form-error">{formData.price.message}</div> 
+                                <div className="dsc-form-error">{formData.price.message}</div>
                             </div>
                             <div>
-                            <FormInput {...formData.imgUrl} 
-                                className="dsc-form-control"
-                                onChange={handleInputChange}
-                                onTurnDirty={handleTurnDirty} 
+                                <FormInput {...formData.imgUrl}
+                                    className="dsc-form-control"
+                                    onChange={handleInputChange}
+                                    onTurnDirty={handleTurnDirty}
                                 />
                             </div>
                             <div>
-                                <FormTextArea {...formData.description} 
-                                className="dsc-form-control dsc-textarea"
-                                onTurnDirty={handleTurnDirty}
-                                onChange={handleInputChange} 
+                                <Select 
+                                options={categories} /* passando a lista de categorias do backendo usando o usaState categorie  */
+                                isMulti
+                                getOptionLabel={(obj: any) => obj.name} /* o rótulo(labl) vai ser o nome */
+                                getOptionValue={(obj: any) => obj.id} /* e o valor vai ser o id  da categoria*//>
+                                
+                            </div>
+                            <div>
+                                <FormTextArea {...formData.description}
+                                    className="dsc-form-control dsc-textarea"
+                                    onTurnDirty={handleTurnDirty}
+                                    onChange={handleInputChange}
                                 />
                                 <div className="dsc-form-error">{formData.description.message}</div>
                             </div>
@@ -154,7 +186,7 @@ export default function ProductForm() {
 
                         <div className="dsc-product-form-buttons">
                             <Link to="/admin/products">
-                            <button type="reset" className="dsc-btn dsc-btn-white">Cancelar</button>
+                                <button type="reset" className="dsc-btn dsc-btn-white">Cancelar</button>
                             </Link>
                             <button type="submit" className="dsc-btn dsc-btn-blue">Salvar</button>
                         </div>
---
+++ src/services/category-service.ts
@@ -0,0 +1,24 @@
+
+// Aqui vai ser um código que terá funcionalidades de négocio relacionadas ao produto
+
+import axios, { AxiosRequestConfig } from "axios";
+import { BASE_URL } from "../utils/system";
+import { requestBackend } from "../utils/requests";
+
+/* retorna todos os produtos */
+
+export function findAllRequest() { /* 1º argumento número da página, 2º nome
+    do produto, 3º qtd de produto na página        */
+    const config : AxiosRequestConfig = { 
+        method: "GET",
+        baseURL: BASE_URL, 
+        url: "/categories"
+    }
+    /*return axios.get(`${BASE_URL}/products/?size=12`); /*tem que importa o axios */
+
+return requestBackend(config);/* config do AxiosRequestConfi vai receber ele mesmo
+mais o baseURL, ai no caso lá no serviço do product chamado findPageRequest, não precisa mais
+colocar a varavel do BASE_URL porque já está configurada no config, é só chamar o requestBackend dentro
+do product-service.t*/
+
+}
